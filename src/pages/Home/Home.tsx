ce5663006e63da542a5522b5a059bfa30d2df5de
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-05-13 22:09:07 +0300

    (feat): add signin form jsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import logo from "../../assets/logo.png";
 import settingsIcon from "../../assets/settings.png";
 
 function Home() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(
+      `Name: ${name}, Password: ${password}, Confirm Password: ${confirmPassword}`
+    );
+  };
   return (
-    <nav className={styles.home_container}>
-      <img src={logo} alt="logo" className={styles.logo} />
-      <div className={styles.details}>
-        <a href="#" className={styles.navigation_link}>
-          About Us
-        </a>
-        <a href="#" className={styles.navigation_link}>
-          Help
-        </a>
-        <a href="#" className={styles.navigation_link}>
-          Feedback
-        </a>
-        <img
-          src={settingsIcon}
-          alt="settings icon"
-          className={styles.settings}
-        />
-      </div>
-    </nav>
+    <div className={styles.home_Wrapper}>
+      <nav className={styles.nav_container}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <div className={styles.details}>
+          <a href="#" className={styles.navigation_link}>
+            About Us
+          </a>
+          <a href="#" className={styles.navigation_link}>
+            Help
+          </a>
+          <a href="#" className={styles.navigation_link}>
+            Feedback
+          </a>
+          <img
+            src={settingsIcon}
+            alt="settings icon"
+            className={styles.settings}
+          />
+        </div>
+      </nav>
+      <form onSubmit={handleSubmit} className={styles.registration_form}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
